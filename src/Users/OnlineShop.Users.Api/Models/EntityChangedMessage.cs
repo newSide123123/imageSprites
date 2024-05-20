3e299f6b664b4f1f81915b1ffb4ed3d654c3be77
newSide123123 <newSide01901@tutamail.com>
2024-05-20 14:16:48 +0300

    fixed validation bugs

---
+++ src/Users/OnlineShop.Users.Api/Models/EntityChangedMessage.cs
@@ -0,0 +1,11 @@
+﻿using OnlineShop.Users.Api.Enums;
+
+namespace OnlineShop.Users.Api.Models;
+
+public class EntityChangedMessage
+{
+    public string EntityName { get; set; }
+    public int EntityId { get; set; }
+    public EntityChangeType ChangeType { get; set; }
+    public string? NewValue { get; set; }
+}
