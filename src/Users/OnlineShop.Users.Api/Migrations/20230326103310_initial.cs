7ca8c7c792f2b428e3f0d365458561f468c3baa6
newSide123123 <newSide01901@tutamail.com>
2024-05-21 14:16:48 +0300

    Improve logging for errors

---
+++ src/Users/OnlineShop.Users.Api/Migrations/20230326103310_initial.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace OnlineShop.Users.Api.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    OrderIds = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "FirstName", "LastName", "OrderIds", "PhoneNumber" },
+                values: new object[,]
+                {
+                    { 1, "johndoe@example.com", "John", "Doe", "[1,2]", "555-555-5555" },
+                    { 2, "janesmith@example.com", "Jane", "Smith", "[3]", "555-555-5555" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
