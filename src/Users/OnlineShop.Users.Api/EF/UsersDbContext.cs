c38706f17fe23e523e0f2ff9088db0c13c15c2d0
newSide123123 <newSide01901@tutamail.com>
2024-05-22 14:16:48 +0300

    fixed css issues

---
+++ src/Users/OnlineShop.Users.Api/EF/UsersDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Users.Api.Models;
+
+namespace OnlineShop.Users.Api.EF;
+
+public class UsersDbContext : DbContext
+{
+	public DbSet<User> Users { get; set; }
+
+	public UsersDbContext()
+	{
+	}
+
+	public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options)
+	{
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		base.OnModelCreating(modelBuilder);
+	}
+}
