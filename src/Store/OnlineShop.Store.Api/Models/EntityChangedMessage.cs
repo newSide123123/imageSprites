b101aa70c6bcfbd1fa93da36d241031edf5fc01f
newSide123123 <newSide01901@tutamail.com>
2024-06-02 14:16:48 +0300

    Add new icons

---
+++ src/Store/OnlineShop.Store.Api/Models/EntityChangedMessage.cs
@@ -0,0 +1,11 @@
+﻿using OnlineShop.Store.Api.Enums;
+
+namespace OnlineShop.Store.Api.Models;
+
+public class EntityChangedMessage
+{
+    public string EntityName { get; set; }
+    public int EntityId { get; set; }
+    public EntityChangeType ChangeType { get; set; }
+    public string? NewValue { get; set; }
+}
