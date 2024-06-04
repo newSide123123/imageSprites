9db2008423704740df1630d5e20a996ffa179d6b
newSide123123 <newSide01901@tutamail.com>
2024-06-04 14:16:48 +0300

    improve logging

---
+++ src/Store/OnlineShop.Store.Api/EF/StoreDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using OnlineShop.Store.Api.Models;
+
+namespace OnlineShop.Store.Api.EF
+{
+    public class StoreDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public StoreDbContext()
+        {
+
+        }
+
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
