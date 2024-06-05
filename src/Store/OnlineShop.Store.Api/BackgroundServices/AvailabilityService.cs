3805a0f09a0cb1743ee5aacc5398e797e5f62cf1
newSide123123 <newSide01901@tutamail.com>
2024-06-05 14:16:48 +0300

    Fix layout issues on Safari

---
+++ src/Store/OnlineShop.Store.Api/BackgroundServices/AvailabilityService.cs
@@ -0,0 +1,16 @@
+﻿namespace OnlineShop.Store.Api.BackgroundServices;
+
+public class AvailabilityService
+{
+    private bool _broken;
+
+    public bool IsBroken()
+    {
+        return _broken;
+    }
+
+    public void Break()
+    {
+        _broken = true;
+    }
+}
