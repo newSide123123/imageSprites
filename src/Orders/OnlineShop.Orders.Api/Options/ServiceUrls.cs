789e200aed7d96b11eb960e55fbfa288694ca710
newSide123123 <newSide01901@tutamail.com>
2024-06-10 14:16:48 +0300

    improve performance

---
+++ src/Orders/OnlineShop.Orders.Api/Options/ServiceUrls.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineShop.Orders.Api.Options
+{
+    public class ServiceUrls
+    {
+        public const string SectionName = "ServiceUrls";
+        public string UsersService { get; set; } = string.Empty;
+    }
+}
