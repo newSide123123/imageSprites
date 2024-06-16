7c34160c8f723853aa0f2290ae3f34f94dbbca6a
newSide123123 <newSide01901@tutamail.com>
2024-06-16 14:16:48 +0300

    update api

---
+++ src/Orders/OnlineShop.Orders.Api/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShop.Orders.Api.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
