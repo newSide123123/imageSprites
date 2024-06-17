30f5286a71ab42b0acc66515b5ca4c756c9f01a7
newSide123123 <newSide01901@tutamail.com>
2024-06-17 14:16:48 +0300

    update readme

---
+++ src/Orders/OnlineShop.Orders.Api/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineShop.Orders.Api.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        public ICollection<OrderItem> Items { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
