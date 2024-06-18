9603bee1c6f9a0faf7b2b2b947512bcdddcc41d4
newSide123123 <newSide01901@tutamail.com>
2024-06-18 14:16:48 +0300

    add new routes

---
+++ src/Orders/OnlineShop.Orders.Api/Models/EntityChangedMessage.cs
@@ -0,0 +1,12 @@
+﻿using OnlineShop.Orders.Api.Enums;
+
+namespace OnlineShop.Orders.Api.Models
+{
+    public class EntityChangedMessage
+    {
+        public string EntityName { get; set; }
+        public int EntityId { get; set; }
+        public EntityChangeType ChangeType { get; set; }
+        public string? NewValue { get; set; }
+    }
+}
