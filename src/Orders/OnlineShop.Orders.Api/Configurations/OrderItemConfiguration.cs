bf233eae0b36a1b330a15386ab8fbc27eaf649f2
newSide123123 <newSide01901@tutamail.com>
2024-06-20 14:16:48 +0300

    update server config

---
+++ src/Orders/OnlineShop.Orders.Api/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Orders.Api.Models;
+
+namespace OnlineShop.Orders.Api.Configurations
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.HasKey(item => item.Id);
+            builder.Property(i => i.Price).HasColumnType("decimal(18,2)");
+        }
+    }
+}
