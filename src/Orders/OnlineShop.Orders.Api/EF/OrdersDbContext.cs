e9199048c9b24bc63143966bf647e0b22cc1bed4
newSide123123 <newSide01901@tutamail.com>
2024-06-20 14:16:48 +0300

    Remove console logs

---
+++ src/Orders/OnlineShop.Orders.Api/EF/OrdersDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.Orders.Api.Models;
+using System.Reflection;
+
+namespace OnlineShop.Orders.Api.EF
+{
+    public class OrdersDbContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public OrdersDbContext()
+        {
+        }
+
+        public OrdersDbContext(DbContextOptions<OrdersDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
