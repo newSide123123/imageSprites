353ccb225c32d733b71ad8679e889b3982670498
newSide123123 <newSide01901@tutamail.com>
2024-06-23 14:16:48 +0300

    Update API endpoints

---
+++ src/Orders/OnlineShop.Orders.Api/Configurations/OrderConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Orders.Api.Models;
+
+namespace OnlineShop.Orders.Api.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.HasMany(p => p.Items)
+                .WithOne(u => u.Order)
+                .HasForeignKey(item => item.OrderId);
+        }
+    }
+}
