8e7b869c40b7e2bdbe0322bbaa7ae60416b073d1
newSide123123 <newSide01901@tutamail.com>
2024-07-02 14:16:48 +0300

    Implement dark mode

---
+++ src/EntityHistory/OnlineShop.EntityHistory.Console/Migrations/20230530080538_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineShop.EntityHistory.Console.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EntityChanges",
+                columns: table => new
+                {
+                    EntityName = table.Column<string>(type: "text", nullable: false),
+                    EntityId = table.Column<int>(type: "integer", nullable: false),
+                    ChangeType = table.Column<int>(type: "integer", nullable: false),
+                    NewValue = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EntityChanges");
+        }
+    }
+}
