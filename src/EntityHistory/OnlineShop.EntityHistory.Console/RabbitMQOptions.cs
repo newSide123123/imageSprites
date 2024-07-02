434092b131069dae9d6333753a94c2de2682eecf
newSide123123 <newSide01901@tutamail.com>
2024-07-02 14:16:48 +0300

    Improve performance of caching

---
+++ src/EntityHistory/OnlineShop.EntityHistory.Console/RabbitMQOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineShop.EntityHistory.Console;
+
+public class RabbitMQOptions
+{
+    public const string SectionName = "RabbitMQ";
+    
+    public string Host { get; set; }
+    public int Port { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string EntityExchange { get; set; }
+    public string EntityCreateQueue { get; set; }
+    public string EntityUpdateQueue { get; set; }
+    public string EntityDeleteQueue { get; set; }
+}
