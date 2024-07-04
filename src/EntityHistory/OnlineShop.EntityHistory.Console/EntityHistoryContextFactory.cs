77f2d2f3a34d320ad970171b002f3af1c89e42d3
newSide123123 <newSide01901@tutamail.com>
2024-07-04 14:16:48 +0300

    fix bug

---
+++ src/EntityHistory/OnlineShop.EntityHistory.Console/EntityHistoryContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace OnlineShop.EntityHistory.Console;
+
+public class EntityHistoryContextFactory : IDesignTimeDbContextFactory<EntityHistoryDbContext>
+{
+    public EntityHistoryDbContext CreateDbContext(string[] args)
+    {
+        var options = new DbContextOptionsBuilder<EntityHistoryDbContext>()
+            .UseNpgsql(
+                "Host=localhost;Port=5433;Database=entityHistory;Username=onlineshop;Password=yhivJp9hroOoY70pASHC")
+            .Options;
+
+        return new EntityHistoryDbContext(options);
+    }
+}
