36c6b255cdbb426d7c51b35fe1b135dd4e1d15f8
newSide123123 <newSide01901@tutamail.com>
2024-07-04 14:16:48 +0300

    Add new tests

---
+++ src/EntityHistory/OnlineShop.EntityHistory.Console/EntityHistoryDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace OnlineShop.EntityHistory.Console;
+
+public class EntityHistoryDbContext : DbContext
+{
+    public DbSet<EntityChangedMessage> EntityChanges { get; set; }
+
+    public EntityHistoryDbContext(DbContextOptions<EntityHistoryDbContext> options) : base(options)
+    {
+    }
+}
