830bf7f4221b7cf10ac73164ba4d703b9e322f0c
newSide123123 <newSide01901@tutamail.com>
2024-07-05 14:16:48 +0300

    add new components

---
+++ src/EntityHistory/OnlineShop.EntityHistory.Console/EntityChangedMessage.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OnlineShop.EntityHistory.Console;
+
+[Keyless]
+public class EntityChangedMessage
+{
+    public string EntityName { get; set; }
+    public int EntityId { get; set; }
+    public EntityChangeType ChangeType { get; set; }
+    public string? NewValue { get; set; }
+}
