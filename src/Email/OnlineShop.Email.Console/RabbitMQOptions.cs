884502e4924107a0869612eb88e374a0012d3832
newSide123123 <newSide01901@tutamail.com>
2024-07-15 14:16:48 +0300

    Improve performance of search

---
+++ src/Email/OnlineShop.Email.Console/RabbitMQOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineShop.Email.Console;
+
+public class RabbitMQOptions
+{
+    public const string SectionName = "RabbitMQ";
+    
+    public string Host { get; set; }
+    public int Port { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string EmailExchange { get; set; }
+    public string EmailSendQueue { get; set; }
+}
