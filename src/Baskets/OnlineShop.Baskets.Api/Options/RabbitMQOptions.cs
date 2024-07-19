437c0fe06c6744828d2915f4788c71a991490b63
newSide123123 <newSide01901@tutamail.com>
2024-07-19 14:16:48 +0300

    Fix bug in password reset

---
+++ src/Baskets/OnlineShop.Baskets.Api/Options/RabbitMQOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace OnlineShop.Baskets.Api.Options;
+
+public class RabbitMQOptions
+{
+    public const string SectionName = "RabbitMQ";
+    
+    public string Host { get; set; }
+    public int Port { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string EntityExchange { get; set; }
+    public string EntityCreateQueue { get; set; }
+    public string EntityUpdateQueue { get; set; }
+    public string EntityDeleteQueue { get; set; }
+    public string EmailExchange { get; set; }
+    public string EmailSendQueue { get; set; }
+}
