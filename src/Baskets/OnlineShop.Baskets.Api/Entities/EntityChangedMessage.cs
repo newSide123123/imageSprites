2bffd5ef0008bf97c3ebcf9392379583bb572522
newSide123123 <newSide01901@tutamail.com>
2024-07-27 14:16:48 +0300

    updated schema

---
+++ src/Baskets/OnlineShop.Baskets.Api/Entities/EntityChangedMessage.cs
@@ -0,0 +1,11 @@
+﻿using OnlineShop.Baskets.Api.Enums;
+
+namespace OnlineShop.Baskets.Api.Models;
+
+public class EntityChangedMessage
+{
+    public string EntityName { get; set; }
+    public int EntityId { get; set; }
+    public EntityChangeType ChangeType { get; set; }
+    public string? NewValue { get; set; }
+}
