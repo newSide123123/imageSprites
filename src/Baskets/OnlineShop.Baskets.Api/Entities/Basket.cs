946ae0f25f7bd6c5874b2d96ee545f9c42258dbe
newSide123123 <newSide01901@tutamail.com>
2024-07-28 14:16:48 +0300

    fixed api bugs

---
+++ src/Baskets/OnlineShop.Baskets.Api/Entities/Basket.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineShop.Baskets.Api.Entities;
+
+public class Basket
+{
+	public int Id { get; set; }
+
+	public int UserId { get; set; }
+
+	public List<int>? BasketProductIds { get; set; }
+}
