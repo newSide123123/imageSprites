3a436aaec80a7d259dde169c9871336c2b5323a4
newSide123123 <newSide01901@tutamail.com>
2024-07-28 14:16:48 +0300

    fixed validation bugs

---
+++ src/Baskets/OnlineShop.Baskets.Api/Entities/BasketsDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineShop.Baskets.Api.Entities;
+
+public class BasketsDbContext : DbContext
+{
+	public BasketsDbContext(DbContextOptions<BasketsDbContext> opts)
+		: base(opts)
+	{
+	}
+
+	public DbSet<Basket> Baskets { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		base.OnModelCreating(modelBuilder);
+	}
+}
