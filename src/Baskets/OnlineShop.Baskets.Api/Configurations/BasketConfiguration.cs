b97d12c5a02ef82f62f257de66281002c391edd9
newSide123123 <newSide01901@tutamail.com>
2024-07-29 14:16:48 +0300

    Update dependencies to latest versions

---
+++ src/Baskets/OnlineShop.Baskets.Api/Configurations/BasketConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineShop.Baskets.Api.Entities;
+
+namespace OnlineShop.Baskets.Api.Configurations;
+
+public class BasketConfiguration : IEntityTypeConfiguration<Basket>
+{
+	public void Configure(EntityTypeBuilder<Basket> builder)
+	{
+		builder.HasKey(b => b.Id);
+
+		var basketProducts = new List<Basket>
+		{
+			new Basket
+			{
+				Id = 1,
+				UserId = 1,
+			},
+			new Basket
+			{
+				Id = 2,
+				UserId = 2,
+			}
+		};
+
+		builder.HasData(basketProducts);
+	}
+}
